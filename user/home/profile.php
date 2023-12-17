f5b2a416023296a56c6f06058311b200d7d92c9a
elizabethceline <elizabethceline04@gmail.com>
2023-12-17 20:35:26 +0700

    anu update tapi style e masih cacat wkwokok

--- user/home/profile.php
+++ user/home/profile.php
@@ -1,136 +1,147 @@
 <?php
-    session_start();
-    if (isset($_SESSION["email"])) {
-        require_once("../../conn.php");
-        $username = $_SESSION["email"];
-        // echo $username;
+session_start();
+if (isset($_SESSION["email"])) {
+    require_once("../../conn.php");
+    $username = $_SESSION["email"];
+    // echo $username;
 
     include '../assets/css/main.html';
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!DOCTYPE html>
+    <html lang="en">
 
-    <!-- Sweet Alert2 -->
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <link href="../assets/css/main.css" rel="stylesheet">
-    <title>User Profile</title>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <style>
-        body {
-            margin-top: 80px;
-        }
+        <!-- Sweet Alert2 -->
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+        <link href="../assets/css/main.css" rel="stylesheet">
+        <title>User Profile</title>
 
-        .hline {
-            width: 90%;
-            border-bottom: 2px solid grey;
-            margin: 3vh;
-        }
+        <style>
+            body {
+                margin-top: 80px;
+            }
 
-        .container {
-            box-shadow: 0 10px 20px rgba(255, 255, 255, 0.35);
-        }
+            .hline {
+                width: 90%;
+                border-bottom: 2px solid grey;
+                margin: 3vh;
+            }
 
-        #change-password{
-            display: none;
-        }
-    </style>
-    <script>
-        $(document).ready(function(){
-            $("#myAccountButton").on("click", function(){
-                $("#changePassButton").removeClass("btn-secondary");
-                $("#changePassButton").addClass("btn-dark");
-                $("#changePassButton").prop("disabled", false);
-                $("#myAccountButton").prop("disabled", true);
-                $("#change-password").hide();
-                $("#profile").show();
+            .container {
+                box-shadow: 0 10px 20px rgba(255, 255, 255, 0.35);
+            }
+
+            #change-password {
+                display: none;
+            }
+
+            @media (max-width: 768px) {
+                #profpic,
+                #profile,
+                #change-password,
+                #main-profile {
+                    border-radius: 0px !important;
+                }
+            }
+        </style>
+        <script>
+            $(document).ready(function() {
+                $("#myAccountButton").on("click", function() {
+                    $("#changePassButton").removeClass("btn-secondary");
+                    $("#changePassButton").addClass("btn-dark");
+                    $("#changePassButton").prop("disabled", false);
+                    $("#myAccountButton").prop("disabled", true);
+                    $("#change-password").hide();
+                    $("#profile").show();
+                })
+
+                $("#changePassButton").on("click", function() {
+                    $("#myAccountButton").removeClass("btn-secondary");
+                    $("#myAccountButton").addClass("btn-dark");
+                    $("#myAccountButton").prop("disabled", false);
+                    $("#changePassButton").prop("disabled", true);
+                    $("#profile").hide();
+                    $("#change-password").show();
+                })
             })
+        </script>
+    </head>
 
-            $("#changePassButton").on("click", function(){
-                $("#myAccountButton").removeClass("btn-secondary");
-                $("#myAccountButton").addClass("btn-dark");
-                $("#myAccountButton").prop("disabled", false);
-                $("#changePassButton").prop("disabled", true);
-                $("#profile").hide();
-                $("#change-password").show();
-            })
-        })
-    </script>
-</head>
-<body>
-    <?php
+    <body>
+        <?php
         include("../navbar.php");
-    ?>
-    <div class="container rounded-4 mt-5">
-        <div class="row">
-            <div class="col-md-4 col-lg-3 bg-white border rounded-start-4">
-                <div class="d-flex flex-column align-items-center text-center p-3 py-3"><img class="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg">
-                    <span class="font-weight-bold">
-                        <?php
+        ?>
+        <div class="container rounded-4 mt-4">
+            <div class="row">
+                <div class="col-md-4 col-lg-3 bg-white border rounded-start-4" id="profpic">
+                    <div class="d-flex flex-column align-items-center text-center p-3 py-3"><img class="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg">
+                        <span class="font-weight-bold">
+                            <?php
                             $stmt = $conn->prepare("SELECT * FROM user WHERE username = ?");
                             $stmt->bind_param("s", $username);
                             $stmt->execute();
                             $result = $stmt->get_result();
-                            if ($result->num_rows > 0){
-                                while ($row = $result->fetch_assoc()){
+                            if ($result->num_rows > 0) {
+                                while ($row = $result->fetch_assoc()) {
                                     echo $row['nama'];
-                                
-                        ?>
-                    </span>
-                    <span class="text-black-50">
-                        <?php
+
+                            ?>
+                        </span>
+                        <span class="text-black-50">
+                            <?php
                                     echo $row['email'];
-                        ?>
-                    </span>
-                    <div class="hline"></div>
-                </div>
-                <div class="d-flex flex-column">
-                    <span style="color: grey;" class="mx-3">Account</p>
-                    <button class="btn btn-secondary d-flex text-right mb-3" style="width: 80%;" id="myAccountButton" disabled>My account</button>
-                    <button class="btn btn-dark d-flex text-right mb-3" style="width: 80%;" id="changePassButton">Change password</button>
-                </div>
-            </div>
-            <div class="col-md-8 col-lg-9 bg-white border rounded-end-4" id="profile">
-                <div class="p-3 py-5">
-                    <div class="d-flex justify-content-between align-items-center mb-3">
-                        <h4 class="text-right">Profile Settings</h4>
+                            ?>
+                        </span>
+                        <div class="hline"></div>
                     </div>
-                    <div class="row mt-3">
-                        <form method="post">
-                            <div class="col-md-12 mt-3"><label class="labels">Name</label><input type="text" name="inputNama" class="form-control" placeholder="Name" value='<?php echo $row['nama'];?>'></div>
-                            <div class="col-md-12 mt-3"><label class="labels">Username</label><input type="text" disabled name="inputUsername" class="form-control" placeholder="Username" value=<?php echo $row['username'];?>></div>
-                            <div class="col-md-12 mt-3"><label class="labels">Email</label><input type="email" disabled name="inputEmail" class="form-control" placeholder="Email" value=<?php echo $row['email'];?>></div>
-                            <div class="col-md-12 mt-3"><label class="labels">Phone number</label><input type="text" name="inputNomorTelp" class="form-control" placeholder="Phone number" value=<?php echo $row['no_telp'];?> minlength="10" maxlength="13"></div>
-                            <div class="col-md-12 mt-3"><label class="labels">Passport number</label><input type="text" disabled name="inputNomorPassport" class="form-control" placeholder="Passport number" value=<?php echo $row['no_passport'];?> minlength="8" maxlength="8"></div>
-                            <div class="mt-5 text-center"><button class="btn btn-dark profile-button" type="submit" id="changeButton">Change Profile</button></div>
-                        </form>
+                    <div class="d-flex flex-column">
+                        <span style="color: grey;" class="mx-3">Account</p>
+                            <button class="btn btn-secondary d-flex text-right mb-3" style="width: 80%;" id="myAccountButton" disabled>My account</button>
+                            <button class="btn btn-dark d-flex text-right mb-3" style="width: 80%;" id="changePassButton">Change password</button>
                     </div>
-                    <?php
-                        }
-                    }
-                    ?>
                 </div>
-            </div>
-            <div class="col-md-8 col-lg-9 border rounded-4" id="change-password">
-                <div class="p-3 py-5">
-                    <div class="d-flex justify-content-between align-items-center mb-3">
-                        <h4 class="text-right">Change Password</h4>
+                <div class="col-md-8 col-lg-9 bg-white border rounded-end-4" id="profile">
+                    <div class="p-3 py-5">
+                        <div class="d-flex justify-content-between align-items-center mb-3">
+                            <h4 class="text-right">Profile Settings</h4>
+                        </div>
+                        <div class="row mt-3">
+                            <form method="post">
+                                <div class="col-md-12 mt-3"><label class="labels">Name</label><input type="text" name="inputNama" class="form-control" placeholder="Name" value='<?php echo $row['nama']; ?>'></div>
+                                <div class="col-md-12 mt-3"><label class="labels">Username</label><input type="text" readonly name="inputUsername" class="form-control" placeholder="Username" value=<?php echo $row['username']; ?>></div>
+                                <div class="col-md-12 mt-3"><label class="labels">Email</label><input type="email" readonly name="inputEmail" class="form-control" placeholder="Email" value=<?php echo $row['email']; ?>></div>
+                                <div class="col-md-12 mt-3"><label class="labels">Phone number</label><input type="text" name="inputNomorTelp" class="form-control" placeholder="Phone number" value=<?php echo $row['no_telp']; ?> minlength="10" maxlength="13"></div>
+                                <div class="col-md-12 mt-3"><label class="labels">Passport number</label><input type="text" readonly name="inputNomorPassport" class="form-control" placeholder="Passport number" value=<?php echo $row['no_passport']; ?> minlength="8" maxlength="8"></div>
+                                <div class="mt-5 text-center"><button class="btn btn-dark profile-button" type="submit" id="changeButton">Change Profile</button></div>
+                            </form>
+                        </div>
+                <?php
+                                }
+                            }
+                ?>
                     </div>
-                    <div class="row mt-3">
-                        <form method="post">
-                            <div class="col-md-12 mt-3"><label class="labels">Old Password</label><input type="password" class="form-control" placeholder="Old Password" value="" name="oldPassword"></div>
-                            <div class="col-md-12 mt-3"><label class="labels">New Password</label><input type="password" class="form-control" placeholder="New Password" value="" name="newPassword"></div>
-                            <div class="mt-5 text-center"><button class="btn btn-dark profile-button" type="submit">Change Password</button></div>
-                        </form>
+                </div>
+                <div class="col-md-8 col-lg-9 border rounded-end-4 bg-white" id="change-password">
+                    <div class="p-3 py-5">
+                        <div class="d-flex justify-content-between align-items-center mb-3">
+                            <h4 class="text-right">Change Password</h4>
+                        </div>
+                        <div class="row mt-3">
+                            <form method="post">
+                                <div class="col-md-12 mt-3"><label class="labels">Old Password</label><input type="password" class="form-control" placeholder="Old Password" value="" name="oldPassword"></div>
+                                <div class="col-md-12 mt-3"><label class="labels">New Password</label><input type="password" class="form-control" placeholder="New Password" value="" name="newPassword"></div>
+                                <div class="mt-5 text-center"><button class="btn btn-dark profile-button" type="submit">Change Password</button></div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-<?php
-    if (isset($_POST["oldPassword"]) && isset($_POST["newPassword"])){
+    <?php
+    if (isset($_POST["oldPassword"]) && isset($_POST["newPassword"])) {
         $stmt = $conn->prepare("SELECT password FROM user WHERE (username = ? OR email = ?) AND password = PASSWORD(?)");
         $stmt->bind_param("sss", $username, $username, $oldPassword);
 
@@ -168,56 +179,57 @@
         header('Location: profile.php');
     }
 } else {
-        header("Location: index.php");
-        exit();
-    }
+    header("Location: index.php");
+    exit();
+}
 
-    $berhasilProfile = '';
-    if (isset($_COOKIE['berhasil-profile'])) {
-        setcookie('berhasil-profile', null, time() - 3600, '/');
-        $berhasilProfile = '<script>
+$berhasilProfile = '';
+if (isset($_COOKIE['berhasil-profile'])) {
+    setcookie('berhasil-profile', null, time() - 3600, '/');
+    $berhasilProfile = '<script>
                 Swal.fire({
                     title: "Profile berhasil diganti!",
                     text: "Kembali ke profile menu",
                     icon: "success"
                 });
             </script>';
-    }
+}
 
-    $berhasilPass = '';
-    if (isset($_COOKIE['berhasil-pass'])) {
-        setcookie('berhasil-pass', null, time() - 3600, '/');
-        $berhasilPass = '<script>
+$berhasilPass = '';
+if (isset($_COOKIE['berhasil-pass'])) {
+    setcookie('berhasil-pass', null, time() - 3600, '/');
+    $berhasilPass = '<script>
                 Swal.fire({
                     title: "Password berhasil diganti!",
                     text: "Kembali ke profile menu",
                     icon: "success"
                 });
             </script>';
-    }
+}
 
-    $gagal = '';
-    if (isset($_COOKIE['gagal'])) {
-        setcookie('gagal', null, time() - 3600, '/');
-        $gagal = '<script>
+$gagal = '';
+if (isset($_COOKIE['gagal'])) {
+    setcookie('gagal', null, time() - 3600, '/');
+    $gagal = '<script>
             Swal.fire({
                 title: "Password Salah!",
                 text: "Inputkan password yang benar!",
                 icon: "error"
             });
         </script>';
-    }
+}
 
-    ob_end_flush();
+ob_end_flush();
     ?>
     <div>
-    <?php
-    if (!empty($berhasilProfile))
-        echo $berhasilProfile;
+        <?php
+        if (!empty($berhasilProfile))
+            echo $berhasilProfile;
 
-    if (!empty($berhasilPass))
-        echo $berhasilPass;
-?>
+        if (!empty($berhasilPass))
+            echo $berhasilPass;
+        ?>
     </div>
-</body>
-</html>
+    </body>
+
+    </html>
